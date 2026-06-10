d56f8b1d65a2a9cc23d5b40478b550c81149a5f5
Pasindu Vihangana <pasi1028@gmail.com>
2026-06-10 19:07:24 +0530

    hotfix/image-loading

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/portfolio" : "",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/portfolio" : "",
   },
 };
 
